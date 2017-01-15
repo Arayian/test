2cf9740672bfc7776110c8bf1621f43be6bf47dd
Arayian <arayian@hotmail.com>
2017-01-15 01:52:37 -0500

    Let's start this

---
+++ Platformer/Platformer/Player.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Platformer
+{
+    class Player
+    {
+        private string _name;
+        private string _class;
+
+        public string Class
+        {
+            get { return _class; }
+            set { _class = value; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+    }
+
+}
+
