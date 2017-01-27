46c9f7fea2bf6fc0c3e401159c8672318582bf70
Arayian <arayian@hotmail.com>
2017-01-27 02:19:37 -0500

    UI Updates

--- NutritionInfoSQL/NutritionInfoSQL/Form1.Designer.cs
+++ NutritionInfoSQL/NutritionInfoSQL/Form1.Designer.cs
@@ -29,9 +29,12 @@
         private void InitializeComponent()
         {
             this.gb_brand = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.lbl_style = new System.Windows.Forms.Label();
+            this.lbl_product = new System.Windows.Forms.Label();
+            this.lbl_brand = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
@@ -44,9 +47,6 @@
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.textBox5 = new System.Windows.Forms.TextBox();
             this.textBox6 = new System.Windows.Forms.TextBox();
-            this.textBox7 = new System.Windows.Forms.TextBox();
-            this.textBox8 = new System.Windows.Forms.TextBox();
-            this.textBox9 = new System.Windows.Forms.TextBox();
             this.label13 = new System.Windows.Forms.Label();
             this.label14 = new System.Windows.Forms.Label();
             this.label15 = new System.Windows.Forms.Label();
@@ -60,14 +60,11 @@
             this.textBox14 = new System.Windows.Forms.TextBox();
             this.textBox15 = new System.Windows.Forms.TextBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.textBox16 = new System.Windows.Forms.TextBox();
-            this.textBox17 = new System.Windows.Forms.TextBox();
-            this.textBox18 = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.cb_gluten = new System.Windows.Forms.CheckBox();
+            this.cb_nitrate = new System.Windows.Forms.CheckBox();
+            this.cb_lowsodium = new System.Windows.Forms.CheckBox();
             this.gb_brand.SuspendLayout();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
@@ -77,41 +74,62 @@
             this.gb_brand.Controls.Add(this.textBox9);
             this.gb_brand.Controls.Add(this.textBox8);
             this.gb_brand.Controls.Add(this.textBox7);
-            this.gb_brand.Controls.Add(this.label3);
-            this.gb_brand.Controls.Add(this.label2);
-            this.gb_brand.Controls.Add(this.label1);
+            this.gb_brand.Controls.Add(this.lbl_style);
+            this.gb_brand.Controls.Add(this.lbl_product);
+            this.gb_brand.Controls.Add(this.lbl_brand);
             this.gb_brand.Location = new System.Drawing.Point(13, 10);
             this.gb_brand.Name = "gb_brand";
             this.gb_brand.Size = new System.Drawing.Size(167, 100);
             this.gb_brand.TabIndex = 0;
             this.gb_brand.TabStop = false;
             // 
-            // label1
+            // textBox9
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 17);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.textBox9.Location = new System.Drawing.Point(54, 67);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 20);
+            this.textBox9.TabIndex = 5;
             // 
-            // label2
+            // textBox8
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(13, 44);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
+            this.textBox8.Location = new System.Drawing.Point(54, 40);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 20);
+            this.textBox8.TabIndex = 4;
             // 
-            // label3
+            // textBox7
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(13, 71);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "label3";
+            this.textBox7.Location = new System.Drawing.Point(54, 13);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 3;
+            // 
+            // lbl_style
+            // 
+            this.lbl_style.AutoSize = true;
+            this.lbl_style.Location = new System.Drawing.Point(13, 71);
+            this.lbl_style.Name = "lbl_style";
+            this.lbl_style.Size = new System.Drawing.Size(30, 13);
+            this.lbl_style.TabIndex = 2;
+            this.lbl_style.Text = "Style";
+            // 
+            // lbl_product
+            // 
+            this.lbl_product.AutoSize = true;
+            this.lbl_product.Location = new System.Drawing.Point(13, 44);
+            this.lbl_product.Name = "lbl_product";
+            this.lbl_product.Size = new System.Drawing.Size(44, 13);
+            this.lbl_product.TabIndex = 1;
+            this.lbl_product.Text = "Product";
+            // 
+            // lbl_brand
+            // 
+            this.lbl_brand.AutoSize = true;
+            this.lbl_brand.Location = new System.Drawing.Point(13, 17);
+            this.lbl_brand.Name = "lbl_brand";
+            this.lbl_brand.Size = new System.Drawing.Size(35, 13);
+            this.lbl_brand.TabIndex = 0;
+            this.lbl_brand.Text = "Brand";
             // 
             // label7
             // 
@@ -208,27 +226,6 @@
             this.textBox6.Name = "textBox6";
             this.textBox6.Size = new System.Drawing.Size(100, 20);
             this.textBox6.TabIndex = 12;
-            // 
-            // textBox7
-            // 
-            this.textBox7.Location = new System.Drawing.Point(54, 13);
-            this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(100, 20);
-            this.textBox7.TabIndex = 3;
-            // 
-            // textBox8
-            // 
-            this.textBox8.Location = new System.Drawing.Point(54, 40);
-            this.textBox8.Name = "textBox8";
-            this.textBox8.Size = new System.Drawing.Size(100, 20);
-            this.textBox8.TabIndex = 4;
-            // 
-            // textBox9
-            // 
-            this.textBox9.Location = new System.Drawing.Point(54, 67);
-            this.textBox9.Name = "textBox9";
-            this.textBox9.Size = new System.Drawing.Size(100, 20);
-            this.textBox9.TabIndex = 5;
             // 
             // label13
             // 
@@ -328,65 +325,14 @@
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.textBox16);
-            this.groupBox1.Controls.Add(this.textBox17);
-            this.groupBox1.Controls.Add(this.textBox18);
-            this.groupBox1.Controls.Add(this.label4);
-            this.groupBox1.Controls.Add(this.label5);
-            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.cb_lowsodium);
+            this.groupBox1.Controls.Add(this.cb_nitrate);
+            this.groupBox1.Controls.Add(this.cb_gluten);
             this.groupBox1.Location = new System.Drawing.Point(186, 9);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(167, 100);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
-            // 
-            // textBox16
-            // 
-            this.textBox16.Location = new System.Drawing.Point(54, 67);
-            this.textBox16.Name = "textBox16";
-            this.textBox16.Size = new System.Drawing.Size(100, 20);
-            this.textBox16.TabIndex = 5;
-            // 
-            // textBox17
-            // 
-            this.textBox17.Location = new System.Drawing.Point(54, 40);
-            this.textBox17.Name = "textBox17";
-            this.textBox17.Size = new System.Drawing.Size(100, 20);
-            this.textBox17.TabIndex = 4;
-            // 
-            // textBox18
-            // 
-            this.textBox18.Location = new System.Drawing.Point(54, 13);
-            this.textBox18.Name = "textBox18";
-            this.textBox18.Size = new System.Drawing.Size(100, 20);
-            this.textBox18.TabIndex = 3;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(13, 71);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
-            this.label4.TabIndex = 2;
-            this.label4.Text = "label3";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(13, 44);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(35, 13);
-            this.label5.TabIndex = 1;
-            this.label5.Text = "label2";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(13, 17);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(35, 13);
-            this.label6.TabIndex = 0;
-            this.label6.Text = "label1";
             // 
             // button1
             // 
@@ -405,6 +351,36 @@
             this.button2.TabIndex = 14;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
+            // 
+            // cb_gluten
+            // 
+            this.cb_gluten.AutoSize = true;
+            this.cb_gluten.Location = new System.Drawing.Point(16, 17);
+            this.cb_gluten.Name = "cb_gluten";
+            this.cb_gluten.Size = new System.Drawing.Size(81, 17);
+            this.cb_gluten.TabIndex = 3;
+            this.cb_gluten.Text = "Gluten Free";
+            this.cb_gluten.UseVisualStyleBackColor = true;
+            // 
+            // cb_nitrate
+            // 
+            this.cb_nitrate.AutoSize = true;
+            this.cb_nitrate.Location = new System.Drawing.Point(16, 42);
+            this.cb_nitrate.Name = "cb_nitrate";
+            this.cb_nitrate.Size = new System.Drawing.Size(81, 17);
+            this.cb_nitrate.TabIndex = 4;
+            this.cb_nitrate.Text = "Nitrate Free";
+            this.cb_nitrate.UseVisualStyleBackColor = true;
+            // 
+            // cb_lowsodium
+            // 
+            this.cb_lowsodium.AutoSize = true;
+            this.cb_lowsodium.Location = new System.Drawing.Point(16, 67);
+            this.cb_lowsodium.Name = "cb_lowsodium";
+            this.cb_lowsodium.Size = new System.Drawing.Size(84, 17);
+            this.cb_lowsodium.TabIndex = 5;
+            this.cb_lowsodium.Text = "Low Sodium";
+            this.cb_lowsodium.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -456,9 +432,9 @@
         private System.Windows.Forms.TextBox textBox9;
         private System.Windows.Forms.TextBox textBox8;
         private System.Windows.Forms.TextBox textBox7;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_style;
+        private System.Windows.Forms.Label lbl_product;
+        private System.Windows.Forms.Label lbl_brand;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label9;
@@ -484,14 +460,11 @@
         private System.Windows.Forms.TextBox textBox14;
         private System.Windows.Forms.TextBox textBox15;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.TextBox textBox16;
-        private System.Windows.Forms.TextBox textBox17;
-        private System.Windows.Forms.TextBox textBox18;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox cb_lowsodium;
+        private System.Windows.Forms.CheckBox cb_nitrate;
+        private System.Windows.Forms.CheckBox cb_gluten;
     }
 }
 
